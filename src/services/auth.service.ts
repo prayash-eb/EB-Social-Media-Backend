5855fb9dcfb9e217c6f38c79565b4ba93f69f522
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-07 16:57:09 +0545

    feat:implemented getUser service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import type { IUser, UserLoginDTO, UserRegisterDTO } from "../interfaces/user.interface.js";
 import { AppError } from "../libs/customError.js";
+import type mongoose from "mongoose";
 
 export class AuthService {
 
@@ -8,11 +9,11 @@ export class AuthService {
         const { email, password } = credentials;
         const userExist = await User.findOne({ email })
         if (!userExist) {
-            throw new AppError("Invalid Credentials", 404)
+            throw new AppError("Invalid Credentials", 404, "AUTH_MOUDLE")
         }
         const isPasswordMatched = userExist.comparePassword(password)
         if (!isPasswordMatched) {
-            throw new AppError("Invalid Credentials", 404)
+            throw new AppError("Invalid Credentials", 404, "AUTH_MODULE")
         }
         const accessToken = userExist.createJWT()
         return accessToken;
@@ -22,11 +23,18 @@ export class AuthService {
         const { name, email, password } = credentials;
         const userExist = await User.findOne({ email });
         if (userExist) {
-            throw new AppError("User with email already exists", 409);
+            throw new AppError("User with email already exists", 409, "AUTH_MODULE");
         }
         const savedUser = await User.insertOne({
             name, email, password
         })
         return savedUser;
+    }
+    public getUser = async (userId: mongoose.Types.ObjectId): Promise<IUser> => {
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new AppError("User not Found", 404, "AUTH_MODULE")
+        }
+        return user
     }
 }
