2303deef9823472b22da1868bd6b2e1672455a04
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-07 16:56:32 +0545

    feat:added route for profile and added middlware as well

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth.controller.js";
 import { AuthService } from "../services/auth.service.js";
+import { Authenticate } from "../middlewares/auth.middleware.js";
 
 const authRouter: Router = Router()
 
@@ -10,10 +11,12 @@ const authController = new AuthController(authService)
 
 authRouter.post("/register", authController.register)
 authRouter.post("/login", authController.login)
+authRouter.get('/profile', Authenticate, authController.profile)
+authRouter.post("/change-password")
+
+
 
 // authRouter.post("/reset-password")
 // authRouter.post("/forgot-password")
-// authRouter.post("/change-password")
-// authRouter.get('/profile')
 
 export default authRouter
