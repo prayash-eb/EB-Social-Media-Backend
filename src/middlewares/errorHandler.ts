35a800ea93ce31bb323f7c0765b4de66718a6dd2
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-08 14:13:53 +0545

    fix:only send errorfrom on dev env

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,13 +3,14 @@ import type { NextFunction, Request, Response } from "express"
 interface CustomError extends Error {
     statusCode?: number
 }
-export const errorHandler = (error: CustomError, req: Request, res: Response, next:NextFunction) => {
+export const errorHandler = (error: CustomError, req: Request, res: Response, next: NextFunction) => {
     const statusCode = error.statusCode || 500
     const message = error.message
     const errorName = error.name
     console.log(`[ERROR_LOG]: ${errorName}-${message}`);
+
     return res.status(statusCode).json({
         message,
-        errorName
+        ...(process.env.NODE_ENV === "dev" ? { errorName } : {})
     })
 }
