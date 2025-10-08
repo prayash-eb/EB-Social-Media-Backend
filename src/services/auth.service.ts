e272a02d16451c66602d7738eebf34b0efdbf326
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-08 09:50:55 +0545

    implemented services for forgot and resetPassword

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,27 +2,29 @@ import User from "../models/user.model.js";
 import type { IUser, UserLoginDTO, UserRegisterDTO } from "../interfaces/user.interface.js";
 import { AppError } from "../libs/customError.js";
 import type mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import { createHash } from "crypto";
 
 export class AuthService {
 
     public login = async (credentials: UserLoginDTO): Promise<string> => {
         const { email, password } = credentials;
-        const userExist = await User.findOne({ email })
-        if (!userExist) {
+        const user = await User.findOne({ email })
+        if (!user) {
             throw new AppError("Invalid Credentials", 404, "AUTH_MOUDLE")
         }
-        const isPasswordMatched = await userExist.comparePassword(password)
+        const isPasswordMatched = await user.comparePassword(password)
         if (!isPasswordMatched) {
             throw new AppError("Invalid Credentials", 404, "AUTH_MODULE")
         }
-        const accessToken = userExist.createJWT()
+        const accessToken = user.createJWT()
         return accessToken;
     }
 
     public register = async (credentials: UserRegisterDTO): Promise<IUser> => {
         const { name, email, password } = credentials;
-        const userExist = await User.findOne({ email });
-        if (userExist) {
+        const user = await User.findOne({ email });
+        if (user) {
             throw new AppError("User with email already exists", 409, "AUTH_MODULE");
         }
         const savedUser = await User.insertOne({
@@ -46,6 +48,43 @@ export class AuthService {
         if (!isOldPasswordCorrect) {
             throw new AppError("Old Password doesnot match", 400, "AUTh_MODULE")
         }
+        user.password = newPassword;
+        await user.save()
+    }
+    public resetPasswordLink = async (email: string): Promise<string> => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AppError("User not Found", 400, "AUTH_MODULE")
+        }
+        // generate resetPassword token using uuid
+        const resetPasswordToken = uuidv4()
+
+        // create a sha256 hash and save it into database
+        const hashedResetPasswordToken = createHash("sha256").update(resetPasswordToken).digest("hex")
+
+        user.resetPasswordToken = hashedResetPasswordToken
+        user.resetPasswordTokenExpiry = new Date(Date.now() + 5 * 60 * 60 * 1000);
+        await user.save()
+
+        return `${process.env.BASE_URL}/api/v1/auth/reset-password?token=${resetPasswordToken}`
+
+    }
+    public resetPassword = async (newPassword: string, resetToken: string): Promise<void> => {
+        const hashedResetPasswordToken = createHash("sha256").update(resetToken).digest("hex")
+        const user = await User.findOne({
+            resetPasswordToken: hashedResetPasswordToken, resetPasswordTokenExpiry: {
+                $gt: new Date(Date.now())
+            }
+        })
+        if (!user) {
+            throw new AppError("Invalid Token", 400, "AUTH_MODULE")
+        }
+
+        // clear the token & expiry time as well
+        user.resetPasswordToken = null;
+        user.resetPasswordTokenExpiry = null;
+
+        // automatically hashed using pre hook on save
         user.password = newPassword;
         await user.save()
     }
