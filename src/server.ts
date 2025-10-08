3c1f25b9895735b2c0eec9f16d941e30fd9349ce
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-08 11:58:30 +0545

    small update

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ config()
 const PORT = process.env.PORT || 5000
 app.listen(PORT, async () => {
     await connectDB()
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running in port:${PORT}`);
 
 })
