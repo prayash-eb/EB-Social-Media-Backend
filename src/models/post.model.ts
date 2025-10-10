281e5bb30b87b8bfd76cb606cec5517f72b939ce
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-10 11:42:47 +0545

    feat:added post schema & model

---
+++ src/models/post.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { model, Schema } from "mongoose";
+import type { IPost } from "../interfaces/post.interface.js";
+
+const postSchema = new Schema<IPost>({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [
+        {
+            commentorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+            comment: { type: String, required: true },
+            createdAt: { type: Date, default: Date.now }
+        }
+    ]
+}, { timestamps: true });
+
+
+const Post = model<IPost>("Post", postSchema)
+export default Post
