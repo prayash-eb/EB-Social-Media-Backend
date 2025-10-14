58dbec5de38b072fc458017280c464e5d991b166
prayash-eb <prayash.mishra@ebpearls.com>
2025-10-14 16:10:47 +0545

    fix:upper limit for single message set to 500 characters

--- src/validators/chat.validator.ts
+++ src/validators/chat.validator.ts
@@ -3,7 +3,7 @@ import { ObjectIdValidator } from "./common.validator.js";
 
 export const sendMessageSchema = z.object({
     receiver: z.string(),
-    message: z.string().min(1)
+    message: z.string().min(1).max(500)
 })
 export const conversationIdSchema = z.object({
     conversationId: ObjectIdValidator
